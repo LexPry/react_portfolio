5c56f4148484c40191948ded05f8f61d73877da3
pryme <akexboswell8@gmail.com>
2022-05-26 18:29:55 -0400

    Adding the rest of the files

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div>
+            <head><title>Alex's page</title></head>
+            <div className={"Head"}>
+                <h1>Alex's Portfolio</h1>
+
+            </div>
+        </div>)
+}
