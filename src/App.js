b019c32fe928a439d6650345c443fc8e80feec2b
pryme <akexboswell8@gmail.com>
2022-05-27 10:59:19 -0400

    Updated descriptions and added new photo for react at the end

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Header from "./Components/Header";
 
 import twitterLogo from "./images/Twitter.png";
 import githubLogo from "./images/github.png";
-import developerPhoto from "./images/me.jpg";
+import developerPhoto from "./images/me.jpg"
+import reactLogo from "./images/react.png";
 
 function App() {
     return (
@@ -41,32 +42,36 @@ function App() {
                                 Boarding Pass Project(CLI)
                             </a>
                             <p className="description">A group project for work that takes in user information, <br/>
-                                and outputs a boarding pass for the trip.</p>
+                                and outputs a boarding pass for the trip with price calculation, regex validation,
+                                <br/> and a local database scanning the files from a csv.</p>
                         </li>
                         <li>
                             <a href="https://github.com/LightingWolf/Genspark-Projects/tree/master/src/java/hangman_reimplementation">
                                 Functional Hangman
                             </a>
-                            <p className="description">Hangman, but in the functional programming style</p>
+                            <p className="description">My second implementation of the game Hangman, <br/>
+                                but in the functional programming style</p>
                         </li>
                         <li>
                             <a href="https://github.com/LightingWolf/Genspark-Projects/tree/master/src/java/goblins">
                                 Goblins Vs Humans(CLI)
                             </a>
-                            <p className="description">A command line game</p>
+                            <p className="description">A command line game where you play as a human, trying to defeat<br/>
+                            goblins by moving around the map, and collecting potions,</p>
                         </li>
                         <li>
                             <a href="https://github.com/LightingWolf/TacoStand">
                                 Taco Stand
                             </a>
-                            <p className="description">A project where I practiced working on Spring, Spring Boot, and
-                                hibernate</p>
+                            <p className="description">A project where I practiced working on Spring, Spring Boot,
+                                hibernate, and PostgresSQL</p>
                         </li>
                     </ul>
                 </div>
 
                 <div id='bottom-bar'>
-                    <p>Created using React</p>
+                    <p>Created this website using React</p>
+                    <img id="reactLogo" src={reactLogo} alt="react logo"/>
                 </div>
             </div>
         </>
